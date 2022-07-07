6e569d50540ed70aa21a05b2b753b425ce451758
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-07 12:46:04 +0000

    [Update] books_index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,20 +7,21 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to root_path
+    redirect_to book_path
   end
 
   def show
   end
 
   def index
+    @books = Book.all
   end
   
   
   private
   
   def book_params
-    params.require(:book).permit(:title,:body,:image)
+    params.require(:book).permit(:title,:body)
   end
   
 end
