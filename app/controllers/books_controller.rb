ea5be04c8ad291bdba8059e9bfa3fdc767419ded
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-07 11:50:58 +0000

    [Add] books_new_create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,25 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to root_path
+  end
 
   def show
   end
 
   def index
   end
+  
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title,:body,:image)
+  end
+  
 end
