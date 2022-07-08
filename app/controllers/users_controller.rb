bf728e4f590a100c50d050f03c0b614eb5cab6f9
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-08 08:04:48 +0000

    [Add] users_edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def edit
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(params[:id])
+  end
 
   def show
+    @user = User.find(params[:id])
   end
+  
+  def index
+    @users = User.all
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:introduction,:profile_image)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about',as: 'about'
   
   resources :books,only: [:new,:show,:index,:create]
+  resources :users,only: [:show,:edit,:index]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
