0c39457911ebfebbea5ec928eb36452bdfb1b171
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-09 13:59:42 +0000

    [Update] sign_inとsign_out後の遷移先

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about',as: 'about'
   
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/registrations#destroy'
+  end
+  
   resources :books,only: [:new,:show,:index,:create]
   resources :users,only: [:show,:edit,:index]
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
