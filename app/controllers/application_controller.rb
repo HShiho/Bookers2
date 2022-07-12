6456e0e56cae9ebbb4dc1f88fceeecc59cd38b2a
HShiho <108495008+HShiho@users.noreply.github.com>
2022-07-12 08:06:39 +0000

    [Update] リンクボタンにfontawesome適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters,if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    books_path
+    about_path
   end
 
   def after_sign_out_path_for(resource)
@@ -10,10 +10,10 @@ class ApplicationController < ActionController::Base
   end
 
 
-  protected
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
   end
 
 end
